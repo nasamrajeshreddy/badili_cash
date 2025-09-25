97762eb1fbd656a381a6512da36c5760170c74a1
nasamrajeshreddy <nasamrajeshreddy@gmail.com>
2025-09-25 21:21:55 +0530

    fix:launching seenumayyas badlicash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,6 @@
 
 @section('content')
 <div class="text-gray-700 text-lg">
-    Welcome to Badili Cash! Use the sidebar to navigate.
+    Welcome to SeenuMayya's Badili Cash!! Use the sidebar to navigate.
 </div>
 @endsection
